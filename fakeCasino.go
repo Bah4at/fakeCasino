ef9b1aeba843c935e6457de55f1e1a71de6c1a13
Nikita <kuldosinn@gmail.com>
2025-06-07 21:57:05 +0500

    Add files via upload

--- fakeCasino.go
+++ fakeCasino.go
@@ -9,14 +9,34 @@ func main() {
 	fmt.Println("Готов проиграть всё, что у тебя есть? Да/Нет")
 	fmt.Scan(&a)
 	if a == "Да" {
-		fmt.Println("Тогда начинаем")
-		/**/
+		bal := 10000
+		fmt.Println("Тогда начинаем. Твой баланс:", bal)
 		for {
 			fmt.Println("Лудка началась")
 			time.Sleep(1 * time.Second)
 			luck := rand.Intn(3)
 			if luck == 0 {
-
+				bal = 0
+				fmt.Println("Ты проиграл всё! Твой баланс равен:", bal)
+				time.Sleep(1 * time.Second)
+				if bal == 0 {
+					break
+				}
+			} else if luck == 1 {
+				fmt.Println("Удача только собирается повернуться к тебе лицом!")
+				fmt.Println("Твой баланс:", bal)
+				time.Sleep(1 * time.Second)
+				if bal == 0 {
+					break
+				}
+			} else {
+				bal = bal * 2
+				fmt.Println("Удача на твоей стороне! Продолжай!")
+				fmt.Println("Твой баланс равен:", bal)
+				time.Sleep(1 * time.Second)
+				if bal == 0 {
+					break
+				}
 			}
 		}
 	} else if a == "Нет" {
